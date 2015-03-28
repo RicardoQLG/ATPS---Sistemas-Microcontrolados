82a1fec04fddbe45e080b484f5c21af1fe6b7401
Ricardo Godoy <rq.godoy@gmail.com>
2015-03-28 20:04:37 -0300

    Remoção de funções para o passo 4

--- main.asm
+++ main.asm
@@ -18,39 +18,9 @@ START BANK1
 
     BANK0
 
-    bcf T1CON,TMR1ON
-    bsf T1CON,T1CKPS1
-    bsf T1CON,T1CKPS0
-    bcf T1CON,T1OSCEN
-    bcf T1CON,TMR1CS
-    clrf TMR1L
-    clrf TMR1H
-    bsf T1CON,TMR1ON
-
-    clrf count
-
 loop
-    call PISCA
+    bsf LED
     goto $-1
-
-PISCA bsf LED
-    call TEMPO
-    bcf LED
-    call TEMPO
-    return
-
-TEMPO btfss PIR1,0
-    goto $-1
-
-    bcf PIR1,0
-    incf count,F
-    bcf T1CON,0
-    movlw 80h
-    movwf TMR1H
-    movlw 0h
-    movwf TMR1L
-    bsf T1CON,0
-    return
 
     END
 
